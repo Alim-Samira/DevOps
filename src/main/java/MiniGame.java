ddcdb6a13c90ac1a8a924c5ad5fb0a194ce4e9d0
Astagabdo Samira <117855051+Alim-Samira@users.noreply.github.com>
2025-11-26 09:53:57 +0000

    Mini Games functionalities

--- src/main/java/MiniGame.java
+++ src/main/java/MiniGame.java
@@ -2,8 +2,6 @@ import java.util.List;
 
 /**
  * Interface de base pour tous les mini-jeux.
- * Cette interface garantit que tout nouveau jeu ajouté aura des méthodes
- * pour démarrer, gérer les entrées des utilisateurs et déterminer s'il est terminé.
  */
 public interface MiniGame {
    
@@ -12,10 +10,10 @@ public interface MiniGame {
     String start();
 
     /**
-     * Traite l'entrée d'un utilisateur pendant que le jeu est actif.
      * @param user L'utilisateur qui a envoyé le message.
      * @param input L'entrée de l'utilisateur (le message envoyé).
-     * @return Le message de réponse du jeu (peut être une mise à jour du score, une nouvelle question, ou un message de fin).
+     * @return Le message de réponse du jeu (peut être une mise à jour du score, une nouvelle question, ou un message de fin). 
+     * A voire
      */
     String processInput(User user, String input);
 
