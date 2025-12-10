176fb7a321d36d6708cf3e9b3f621430d5bf4651
Léa MOUGEL <44008242@parisnanterre.fr>
2025-12-10 12:21:19 +0100

    1ere étape de mise en place de springboot avec un controleur test qui affiche un message

---
+++ src/main/java/api/TestController.java
@@ -0,0 +1,24 @@
+package api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Ce contrôleur vérifie que l'injection de dépendances et le serveur REST
+ * fonctionnent correctement dans le contexte de votre projet DevOps.
+ */
+@RestController 
+public class TestController {
+
+    // Mappe cette méthode à l'URL de base : http://localhost:8080/
+    @GetMapping("/")
+    public String checkStatus() {
+        return "Serveur REST du Projet DevOps en ligne. Prêt pour les WatchParties.";
+    }
+    
+    // Mappe cette méthode à l'URL : http://localhost:8080/test
+    @GetMapping("/test")
+    public String getProjectTest() {
+        return "Validation du chemin Spring Boot OK !";
+    }
+}
